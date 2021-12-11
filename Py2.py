0ba534d5b920462138ce8fc95f1781aad3027bcb
Ayub Black <ayub.gadiev95@gmail.com>
2021-12-11 20:45:30 +0300

    0.1.1

---
+++ Py2.py
@@ -0,0 +1,10 @@
+n = int(input('Введите длину списка:'))
+user_list =[]
+
+i=0
+while i < n:
+    string = "Введите номер элемента #" + str(i + 1) + " :"
+    user_list.append(input(string))
+    i += 1
+
+print(user_list)
