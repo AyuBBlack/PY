1c871d092400d3d0e918a81eda616f3b084812ff
Ayub Black <ayub.gadiev95@gmail.com>
2021-12-20 10:43:29 +0300

    0.14.1 summa try except

--- MyPy.py
+++ MyPy.py
@@ -1,7 +1,14 @@
-x = 0
-while x == 0:
+first_numb = 0
+while first_numb == 0:
     try:
-        x = int(input('Введите число: '))
-        print(x)
+        first_numb = int(input('Введите первое число: '))
+        second_numb = int(input('Введите второе число: '))
+
+        def summa(c, d):
+            return c + d
     except ValueError:
         print('Введите целое число')
+    else:
+        print('Сумма чисел:', summa(first_numb, second_numb))
+    finally:
+        print("Сработал Finally")
