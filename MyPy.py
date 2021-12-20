44dfb16a512e18161e34120e066f19ef42aafd3d
Ayub Black <ayub.gadiev95@gmail.com>
2021-12-20 10:11:05 +0300

    0.13 files -w -r

--- MyPy.py
+++ MyPy.py
@@ -1,18 +1,14 @@
-def minimal(lis):
-    min_numb = lis[0]
-    for el in lis:
-        if el < min_numb:
-            min_numb = el
-    return min_numb
+# data = input('Введите текст: ')
+# file = open('../data/text.txt', 'w')
+#
+# file.write(data)
+#
+# file.close()
 
+file = open('../data/text.txt', 'r')
 
-nums = [2, 5, 6, 5, 2, 7]
-min_nums = minimal(nums)
-nums_2 = [1.2, 5.5, 6, 43]
-min_nums_2 = minimal(nums_2)
+# print(file.read())
 
-
-if min_nums < min_nums_2:
-    print(min_nums)
-else:
-    print(min_nums_2)
+for line in file:
+    print(line, end='')
+file.close()
