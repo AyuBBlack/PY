d4119c30fcee9b80e32fd79050c59a49498aee21
Ayub Black <ayub.gadiev95@gmail.com>
2021-12-22 15:48:24 +0300

    0.17 class

--- MyPy.py
+++ MyPy.py
@@ -1,3 +1,18 @@
-from firstmod import addNumbers as add
+class cat:
+    name = None
+    age = None
+    isHappy = None
 
-print(add(1, 2, 0))
+
+cat_1 = cat()
+cat_1.name = "Boris"
+cat_1.age = 1
+cat_1.isHappy = True
+
+cat_2 = cat()
+cat_2.name = "Lupen"
+cat_2.age = 2
+cat_2.isHappy = False
+
+print(cat_1.name)
+print(cat_2.name)
