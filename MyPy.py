4a684ec52fb260d68d0382f0b49878375b5fe4ef
AyuBBlack <ayub.gadiev95@gmail.com>
2022-01-02 11:32:14 +0300

    0.20 decorator

--- MyPy.py
+++ MyPy.py
@@ -1,36 +1,18 @@
-class Building:
-    __year = None
-    __city = None
-
-    def __init__(self, year, city):
-        self.year = year
-        self.city = city
-
-    def get_info(self):
-        print("Year:", self.year, ". City:", self.city)
+import webbrowser
 
 
-class School(Building):
-    pupils = 0
-
-    def __init__(self, pupils, year, city):
-        super(School, self).__init__(year, city)
-        self.pupils = pupils
-
-    def get_info(self):
-        super().get_info()
-        print('Pupils:', self.pupils)
+def validator(func):
+    def wrapper(url):
+        if "." in url:
+            func(url)
+        else:
+            print("Неверный URL")
+    return wrapper
 
 
-class House(Building):
-    pass
+@validator
+def open_url(url):
+    webbrowser.open(url)
 
 
-class Shop(Building):
-    pass
-
-
-school = School(500, 2021, "Grozny")
-school.get_info()
-house = House(2000, "Grozny")
-shop = Shop(2015, "Grozny")
+open_url("https://google.com")
