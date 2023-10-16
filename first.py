ddf488910c34dba8ba6c2f36916987b7fb8f3a2e
AyuBBlack <ayub.gadiev95@gmail.com>
2023-10-16 13:40:52 +0300

    Replace

--- first.py
+++ first.py
@@ -1,12 +1,3 @@
-# li = [1, 4, 3, 4, 1]
-# li.sort()
-# dict = {'age': 15, 'Name': 'Ayub'}
-
-# def my_func (a, b):
-#     a = a + 2
-#     c = a + b
-#     return c
-
 name = input("Введите значение: ")
 
-print (name.upper())
+print (name.replace('u', 'a'))
