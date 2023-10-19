e1d3179555b7bc7e92175fa666d57ad4ac1299ac
AyuBBlack <ayub.gadiev95@gmail.com>
2023-10-19 17:50:41 +0300

    Long_str

--- first.py
+++ first.py
@@ -1,12 +1,5 @@
-name = input("Имя: ")
+long_str = """This is long 
 
-
-# Функция вывода текста
-def print_name(x):
-    print(x)
-
-
-print_name(name)
-print(id(print_name))
-new_var = print_name
-print(id(new_var))
+string"""
+print(long_str)
+print(type(long_str))
